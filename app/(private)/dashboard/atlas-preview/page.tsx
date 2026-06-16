1f0e923ef13497ae93e2a828d3ef74b4fb7981b6
ejd5 <31916537+ejd5@users.noreply.github.com>
2026-06-16 23:42:28 +0200

    Add Atlas premium command center preview

---
+++ app/(private)/dashboard/atlas-preview/page.tsx
@@ -0,0 +1,9 @@
+import { AtlasCommandCenter } from "@/components/atlas-command-center/AtlasCommandCenter";
+
+export default function AtlasCommandCenterPreviewPage() {
+  return (
+    <div className="-m-4 md:-m-8">
+      <AtlasCommandCenter />
+    </div>
+  );
+}
