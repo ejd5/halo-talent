d621e505c5ba4051f8eabf13dbaf9a86aa9e037b
ejd5 <31916537+ejd5@users.noreply.github.com>
2026-06-17 00:54:19 +0200

    Render Atlas preview without dashboard offset

--- app/(private)/dashboard/atlas-preview/page.tsx
+++ app/(private)/dashboard/atlas-preview/page.tsx
@@ -1,9 +1,5 @@
 import { AtlasCommandCenter } from "../../../../components/atlas-command-center/AtlasCommandCenter";
 
 export default function AtlasCommandCenterPreviewPage() {
-  return (
-    <div className="-m-4 md:-m-8">
-      <AtlasCommandCenter />
-    </div>
-  );
+  return <AtlasCommandCenter />;
 }
